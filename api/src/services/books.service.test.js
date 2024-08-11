997e9a10133483d744238cc13142468c4fdbacdf
amr89.dev <amr89.dev@gmail.com>
2024-08-10 21:29:03 -0500

    add mock for test

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,17 +1,43 @@
 const service = require("./books.service");
 
+const fakeBooks = [
+  {
+    id: "1",
+    title: "Book 1",
+  },
+  {
+    id: "2",
+    title: "Book 2",
+  },
+  {
+    id: "3",
+    title: "Book 3",
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock("../lib/mongo.lib", () =>
+  jest.fn().mockImplementation(() => MongoLibStub)
+);
+
 describe("Test for books service", () => {
   let BooksService;
 
   //Arrange
   beforeEach(() => {
     BooksService = new service();
+    jest.clearAllMocks();
   });
   //Act
 
   test("should return an array of books", async () => {
-    const books = await BooksService.getBooks({});
-    expect(books.length).toEqual(2);
+    const books = await BooksService.getBooks();
+    console.log(books);
+    expect(books.length).toEqual(1);
   });
   //Assert
 });
